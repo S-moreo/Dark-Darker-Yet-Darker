7c1a69f52adbd34040a5ec4065336f82ebe0eca3
S-moreo <78574820+S-moreo@users.noreply.github.com>
2023-10-20 23:50:06 -0400

    Initial commit
    Never back down! Never what!?

---
+++ src/client/java/net/smoreo/ddyd/DDYDClient.java
@@ -0,0 +1,10 @@
+package net.smoreo.ddyd;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DDYDClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/smoreo/ddyd/DDYD.java
@@ -0,0 +1,16 @@
+package net.smoreo.ddyd;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DDYD implements ModInitializer {
+	public static final String MOD_ID = "ddyd";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
