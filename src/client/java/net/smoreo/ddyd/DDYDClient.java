0f23d2881fd9bd70df3334d3507578998011a1c6
S-moreo <78574820+S-moreo@users.noreply.github.com>
2023-10-22 18:30:28 -0400

    Added GeckoLib and PlayerAnimator

--- src/client/java/net/smoreo/ddyd/DDYDClient.java
+++ src/client/java/net/smoreo/ddyd/DDYDClient.java
@@ -1,10 +1,15 @@
 package net.smoreo.ddyd;
 
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationFactory;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.resource.ResourceLocation; //Doesn't exist??
 
 public class DDYDClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		PlayerAnimationFactory.ANIMATION_DATA_FACTORY.registerFactory(
+				new ResourceLocation()//How tf do you init animations
+		)
 	}
 }
--- src/main/java/net/smoreo/ddyd/DDYD.java
+++ src/main/java/net/smoreo/ddyd/DDYD.java
@@ -1,10 +1,12 @@
 package net.smoreo.ddyd;
 
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationFactory;
 import net.fabricmc.api.ModInitializer;
 
 import net.smoreo.ddyd.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.bernie.geckolib3.GeckoLib;
 
 public class DDYD implements ModInitializer {
 	public static final String MOD_ID = "ddyd";
@@ -14,6 +16,8 @@ public class DDYD implements ModInitializer {
 	public void onInitialize() {
 
 		ModItems.registerModItems();
+
+		GeckoLib.initialize();
 
 	}
 }
